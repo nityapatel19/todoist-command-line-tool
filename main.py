98a0031c3fb39800a5608d573215d64c06b6e47f
nityapatel19 <nityapatel19@gmail.com>
2022-09-11 12:22:11 +0530

    Added command 'find' and 'done'

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import json
 from enum import IntEnum
-
+from pprint import pprint
+from prettytable import PrettyTable
 
 from todoist_api_python.api import TodoistAPI
 import typer
-
-# from utils import *
 
 
 class Priority(IntEnum):
@@ -21,12 +20,36 @@ app = typer.Typer()
 
 
 @app.command()
-
+def add(name: str, time=None, date=None, priority: int = Priority.p4.value, description: str = '',
+        project: int = None):
+    todo.add_task(name, priority=priority, project=project, due_date_utc=date, due_time_utc=time,
+                  description=description)
 
 
 @app.command()
-def add_task(name: str, time=None, date=None, priority: int=Priority.p4.value, description: str='', project: int=None):
-    todo.add_task(name, priority=priority, project=project, due_date_utc=date, due_time_utc=time, description=description)
+def done(name: str):
+    tasks = todo.get_tasks()
+    for task in tasks:
+        if task.content == name:
+            todo.close_task(task.id)
+            break
+
+
+@app.command()
+def find(key: str):
+    tasks = todo.get_tasks()
+    task_table = PrettyTable(['Sr. No.', 'Name', 'Priority', 'Due Date', 'Project', 'Description'])
+    sr_no = 0
+    for task in tasks:
+        if key in task.content:
+            sr_no += 1
+            name = task.content
+            priority = Priority(1).name
+            due_date = task.due.string
+            project = todo.get_project(task.project_id).name
+            description = task.description
+            task_table.add_row([sr_no, name, priority, due_date, project, description])
+    print(task_table)
 
 
 @app.command()
@@ -39,14 +62,13 @@ def show_all():
             tree[task.project_id] = {}
             tree[task.project_id]['name'] = todo.get_project(task.project_id).name
             tree[task.project_id]['tasks'] = [task.content]
-    print(json.dumps(tree, indent=4))
+    # print(json.dumps(tree, indent=4))
+    pprint(tree)
 
 
 if __name__ == '__main__':
-    # app()
-    todo.add_task("Test", priority=Priority.p2.value)
-    show_all()
-
+    app()
+    # todo.add_task("Test", priority=Priority.p2.value)
 
 # def get_tree(todo: TodoistAPI) -> dict:
 #     tree = {}
