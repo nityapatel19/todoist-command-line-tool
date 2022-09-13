5abc876fed3f0587d2da614cd7b95f7315ddb278
nityapatel19 <nityapatel19@gmail.com>
2022-09-13 22:17:22 +0530

    Implemented OOPS

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import logging
 from enum import IntEnum
 from pprint import pprint
+from typing import Optional
 
 import dotenv
 import typer
@@ -15,70 +17,92 @@ class Priority(IntEnum):
     p4 = 1
 
 
-dotenv.load_dotenv()
-todo = TodoistAPI(os.getenv('API_TOKEN'))
+logger = logging.getLogger(__name__)
+# dotenv.load_dotenv()
+todo = 'todoist'
 app = typer.Typer()
 
 
-@app.command()
-def add(name: str, time=None, date=None, priority: int = Priority.p4.value, description: str = '',
-        project: int = None):
-    todo.add_task(name, priority=priority, project=project, due_date_utc=date, due_time_utc=time,
-                  description=description)
+def set_token():
+    token = input("Enter your API token: ")
+    dotenv.set_key(dotenv.find_dotenv(), 'API_TOKEN', token)
+    return token
 
 
-@app.command()
-def done(name: str):
-    tasks = todo.get_tasks()
-    for task in tasks:
-        if task.content == name:
-            print(f"{name} completed.")
-            todo.close_task(task.id)
-            break
+class Todoist:
+    def __init__(self):
+        self._client: Optional[TodoistAPI] = None
+
+    @property
+    def client(self):
+        if self._client is None:
+            token = os.getenv('API_TOKEN')
+            if not token:
+                dotenv.load_dotenv()
+                token = os.getenv('API_TOKEN')
+            if not token:
+                token = set_token()
+            self._client = TodoistAPI(token)
+        return self._client
+
+    def add(self, name: str, time=None, date=None, priority: int = Priority.p4.value, description: str = '',
+            project: int = None):
+        self.client.add_task(name, priority=priority, project=project, due_date_utc=date, due_time_utc=time,
+                             description=description)
+
+    def done(self, name: str):
+        tasks = self.client.get_tasks()
+        for task in tasks:
+            if task.content == name:
+                print(f"{name} completed.")
+                self.client.close_task(task.id)
+                break
+
+    def find_project(self, key: str):
+        projects = self.client.get_projects()
+        project_table = PrettyTable(['Sr. No.', 'Name'])
+        sr_no = 0
+        for project in projects:
+            if key in project.name:
+                sr_no += 1
+                name = project.name
+                project_table.add_row([sr_no, name])
+        print(project_table)
+
+    def list_all(self):
+        tree = {}
+        for task in self.client.get_tasks():
+            if task.project_id in tree:
+                tree[task.project_id]['tasks'].append(task.content)
+            else:
+                tree[task.project_id] = {}
+                tree[task.project_id]['name'] = self.client.get_project(task.project_id).name
+                tree[task.project_id]['tasks'] = [task.content]
+        pprint(tree)
+
+    def find(self, key: str):
+        tasks = self.client.get_tasks()
+        task_table = PrettyTable(['Sr. No.', 'Name', 'Priority', 'Due Date', 'Project', 'Description'])
+        sr_no = 0
+        for task in tasks:
+            if key in task.content:
+                sr_no += 1
+                name = task.content
+                priority = Priority(1).name
+                due_date = task.due.string if task.due else ""
+                project = self.client.get_project(task.project_id).name
+                description = task.description
+                task_table.add_row([sr_no, name, priority, due_date, project, description])
+        print(task_table)
 
 
-@app.command()
-def find(key: str):
-    tasks = todo.get_tasks()
-    task_table = PrettyTable(['Sr. No.', 'Name', 'Priority', 'Due Date', 'Project', 'Description'])
-    sr_no = 0
-    for task in tasks:
-        if key in task.content:
-            sr_no += 1
-            name = task.content
-            priority = Priority(1).name
-            due_date = task.due.string
-            project = todo.get_project(task.project_id).name
-            description = task.description
-            task_table.add_row([sr_no, name, priority, due_date, project, description])
-    print(task_table)
-
-
-@app.command()
-def find_project(key: str):
-    projects = todo.get_projects()
-    project_table = PrettyTable(['Sr. No.', 'Name'])
-    sr_no = 0
-    for project in projects:
-        if key in project.name:
-            sr_no += 1
-            name = project.name
-            project_table.add_row([sr_no, name])
-    print(project_table)
-
-
-@app.command()
-def list_all():
-    tree = {}
-    for task in todo.get_tasks():
-        if task.project_id in tree:
-            tree[task.project_id]['tasks'].append(task.content)
-        else:
-            tree[task.project_id] = {}
-            tree[task.project_id]['name'] = todo.get_project(task.project_id).name
-            tree[task.project_id]['tasks'] = [task.content]
-    pprint(tree)
-
+todoist = Todoist()
+app.command()(set_token)
+app.command()(todoist.add)
+app.command()(todoist.done)
+app.command()(todoist.find)
+app.command()(todoist.find_project)
+app.command(name='list')(todoist.list_all)
 
 if __name__ == '__main__':
     app()
